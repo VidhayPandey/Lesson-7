687f3f682fde4be77c26722a92af2ab8b6ab8f73
VidhayPandey <poojapandey0621@gmail.com>
2026-05-11 11:52:34 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,43 @@
+print("Enter Marks Obtained in 6 Subjects:")
+
+m1= int(input("Maths:"))
+m2= int(input("Science:"))
+m3= int(input("English:"))
+m4= int(input("Hindi:"))
+m5= int(input("Computer:"))
+m6= int(input("Spanish:"))
+
+tot= m1+m2+m3+m4+m5+m6
+avg = int(tot/6)
+
+validRange = range(0,101)
+
+if avg not in validRange:
+    print("Invalid Input!")
+
+elif avg in range(91,101):
+    print("Your grade is A1")
+
+elif avg in range(81,91):
+    print("Your grade is A2")
+
+elif avg in range(71,81):
+    print("Your grade is B1")
+
+elif avg in range(61,71):
+    print("Your grade is B2")
+
+elif avg in range(51,61):
+    print("Your grade is C1")
+
+elif avg in range(41,51):
+    print("Your grade is C2")
+
+elif avg in range(33,41):
+    print("Your grade is D")
+
+elif avg in range(21,33):
+    print("Your grade is E1")
+
+elif avg in range(0,21):
+    print("Your grade is E2")
